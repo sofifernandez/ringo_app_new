aee9097a42a1df4f31b0afb57340a577be1f3f3f
Sofia Fernandez <sofiafernandez03@gmail.com>
2021-11-03 08:57:49 -0300

    correcciones

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-//import {NavBar} from './components/NavBar/NavBar.js'
-//import { CartWidget } from "./components/CartWidget/CartWidget"
+import {NavBar} from './components/NavBar/NavBar.js'
+import { CartWidget } from "./components/CartWidget/CartWidget"
 import { Home } from "./components/Home/Home"
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer"
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer"
-
+//import { Category } from "./components/Category/Category"
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <NavBar>
+      <NavBar>
         <CartWidget contadorCarrito='?' /> 
-      </NavBar> */}
+      </NavBar>
       <Switch>
         <Route exact path='/ringo_app'>
           <Home/>
